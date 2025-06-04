1f1bd43bd5aa814682bd7c518b6060a8a65a07dd
amonuddin <amonuddinm@gmail.com>
2025-06-04 09:52:25 +0530

    frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to SalesSync!</h1>
+    </div>
+  );
+}
+
+export default Home;
